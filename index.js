b73ee9f321854dbc47a1e0e59f470cc753386554
Gisele Xiao <yunshu0507@gmail.com>
2021-02-22 12:53:48 -0500

    save users to the mangoDB

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
-require('./server/services/passport');
 const oauthRoutes = require('./server/routes/oauthRoutes');
+const mongoose = require('mongoose');
+const keys = require('./server/config/keys.js');
+require('./server/models/Users');
+require('./server/services/passport');
 
+//handle mangoDB
+mongoose.connect(keys.mongoURI);
+
+//handle google OAuth
 const app = express();
-
 oauthRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,12 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
   clientID : keys.googleClientID,
   clientSecret : keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) =>{
-  console.log(accessToken);
-  console.log(accessToken);
+},
+(accessToken, refreshToken, profile, done) =>{
+  User.findOne({ googleId : profile.id })
+    .then((existingUser) => {
+      if (existingUser){
+        //already have a record;
+      } else {
+        //don't have a record with this UserID;
+        new User({
+          googleId : profile.id
+        }).save();
+      }
+    })
+
+
 }));
